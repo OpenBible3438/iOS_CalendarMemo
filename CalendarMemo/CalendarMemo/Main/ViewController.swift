762207d95800d1f7886d584b85cc6f133dccae5d
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-11-09 20:35:28 +0900

    feat: Calendar init setting code

--- CalendarMemo/CalendarMemo/Main/ViewController.swift
+++ CalendarMemo/CalendarMemo/Main/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    
+    @IBOutlet weak var calendarView: FSCalendar!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setCalendar()
     }
 
+    // Init FSCalendar
+    func setCalendar() {
+        self.calendarView.delegate = self
+        self.calendarView.dataSource = self
+        
+        // 달력 형태 : 월간
+        self.calendarView.scope = .month
+        
+        // 요일 출력 언어
+        self.calendarView.locale = Locale(identifier: "ko_KR")
+        
+        // 요일 글자 색
+        self.calendarView.appearance.weekdayTextColor = UIColor.gray
+        
+        // 요일 날짜 색
+        self.calendarView.appearance.todayColor = UIColor.blue
+        
+        // 선택된 날짜 색
+        self.calendarView.appearance.selectionColor = UIColor.gray
+        
+        // 헤더 양 옆의 글씨 투명도 (안 보이게)
+        self.calendarView.appearance.headerMinimumDissolvedAlpha = 0.0
+        
+        // 헤더 폰트 색상
+        self.calendarView.appearance.headerTitleColor = UIColor.gray
+        
+    }
 
 }
 
